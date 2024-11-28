8a514ef4c27a40ab63f4f66f31b400960377c8c1
Armes, James <james.armes@medtronic.com>
2024-11-28 14:48:22 -0700

    Add in some test code to play around with some concepts

--- process_pilot/process.py
+++ process_pilot/process.py
@@ -1,8 +1,22 @@
 import json
+import subprocess
+from enum import Enum
 from pathlib import Path
 
 import yaml
 from pydantic import BaseModel
+
+
+class ProcessStrategy(str, Enum):
+    RESTART = "restart"  # Restart the service in question
+    DO_NOT_RESTART = "do_not_restart"  # Leave it dead
+    SHUTDOWN_EVERYTHING = "shutdown_everything"  # Take down everything else with it
+
+
+class ProcessHooks(str, Enum):
+    ON_START = "on_start"
+    ON_SHUTDOWN = "on_shutdown"
+    ON_RESTART = "on_restart"
 
 
 class Process(BaseModel):
@@ -27,3 +41,35 @@ class ProcessManifest(BaseModel):
             yaml_data = yaml.safe_load(f)
 
         return cls(**yaml_data)
+
+
+class ProcessPilot:
+    def __init__(self, manifest: ProcessManifest) -> None:
+        self.manifest = manifest
+
+        self.running_processes: list[subprocess.Popen[str]] = []
+
+    def start(self) -> None:
+        for entry in self.manifest.processes:
+            command = [str(entry.path)] if not entry.args else [str(entry.path), *entry.args]
+
+            x = subprocess.Popen(command, encoding="utf-8")
+
+            self.running_processes.append(x)
+
+        print("x")
+
+        while True:
+            y = self.running_processes[0].poll()
+            if y is not None:
+                print("DONE")
+                print(y)
+                print(self.running_processes[0].communicate())
+                break
+
+
+if __name__ == "__main__":
+    manifest = ProcessManifest.from_json(Path(__file__).parent.parent / "tests" / "examples" / "services.json")
+    pilot = ProcessPilot(manifest)
+
+    pilot.start()
