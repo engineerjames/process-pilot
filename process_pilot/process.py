b35fe4bf9ee7cb2f0ec513598197194873795fd1
Armes, James <james.armes@medtronic.com>
2024-12-20 21:46:23 -0700

    More updates to the basic implementaton

--- process_pilot/process.py
+++ process_pilot/process.py
@@ -2,12 +2,13 @@ import json
 import subprocess
 from enum import Enum
 from pathlib import Path
+from time import sleep
 
 import yaml
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
-class ProcessStrategy(str, Enum):
+class ShutdownStrategy(str, Enum):
     RESTART = "restart"  # Restart the service in question
     DO_NOT_RESTART = "do_not_restart"  # Leave it dead
     SHUTDOWN_EVERYTHING = "shutdown_everything"  # Take down everything else with it
@@ -21,8 +22,10 @@ class ProcessHooks(str, Enum):
 
 class Process(BaseModel):
     path: Path
-    args: list[str] | None = None
+    args: list[str] = Field(default=[])
     timeout: float | None = None
+    shutdown_strategy: ShutdownStrategy | None = ShutdownStrategy.RESTART
+    dependencies: list["Process"] = Field(default=[])
 
 
 class ProcessManifest(BaseModel):
@@ -44,28 +47,58 @@ class ProcessManifest(BaseModel):
 
 
 class ProcessPilot:
-    def __init__(self, manifest: ProcessManifest) -> None:
+    def __init__(self, manifest: ProcessManifest, poll_interval: float = 0.1) -> None:
         self.manifest = manifest
-
-        self.running_processes: list[subprocess.Popen[str]] = []
+        self.poll_interval = poll_interval
+        self._processes: list[tuple[Process, subprocess.Popen[str]]] = []
+        self._shutting_down: bool = False
 
     def start(self) -> None:
-        for entry in self.manifest.processes:
-            command = [str(entry.path)] if not entry.args else [str(entry.path), *entry.args]
+        """ """
+        try:
+            for entry in self.manifest.processes:
+                command = [str(entry.path), *entry.args]
+                new_popen_result = subprocess.Popen(command, encoding="utf-8")
+                self._processes.append((entry, new_popen_result))
 
-            x = subprocess.Popen(command, encoding="utf-8")
+            while not self._shutting_down:
+                for process_entry, process in self._processes:
+                    result = process.poll()
+                    if result is None:
+                        continue
 
-            self.running_processes.append(x)
+                    # exited
+                    # log exited process
+                    # need to remove exited process
+                    match process_entry.shutdown_strategy:
+                        case ShutdownStrategy.SHUTDOWN_EVERYTHING:
+                            self.stop()
+                        case ShutdownStrategy.DO_NOT_RESTART:
+                            pass
+                        case ShutdownStrategy.RESTART:
+                            self._processes.append(
+                                (
+                                    process_entry,
+                                    subprocess.Popen([str(process_entry.path), *process_entry.args], encoding="utf-8"),
+                                )
+                            )
+                        case _:
+                            pass
 
-        print("x")
+                sleep(self.poll_interval)
+        except KeyboardInterrupt:
+            self.stop()
 
-        while True:
-            y = self.running_processes[0].poll()
-            if y is not None:
-                print("DONE")
-                print(y)
-                print(self.running_processes[0].communicate())
-                break
+    def stop(self) -> None:
+        self._shutting_down = True
+
+        for process_entry, process in self._processes:
+            process.terminate()
+
+            try:
+                process.wait(process_entry.timeout)
+            except subprocess.TimeoutExpired:
+                process.kill()
 
 
 if __name__ == "__main__":
