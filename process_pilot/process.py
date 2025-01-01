7c2ee5b44c35d8058519bac1a3f3e2f0b8cd14ca
Armes, James <james.armes@medtronic.com>
2024-12-31 21:05:27 -0700

    Fix linting issues

--- process_pilot/process.py
+++ process_pilot/process.py
@@ -143,7 +143,8 @@ class Process(BaseModel):
         self,
         ready_strategies: dict[str, Callable[["Process", float], bool]],
     ) -> bool:
-        """Wait for process to signal readiness."""  # TODO: Don't think we need to wait for processes that have no dependents
+        """Wait for process to signal readiness."""
+        # TODO: Don't think we need to wait for processes that have no dependents
         if self.ready_strategy not in ready_strategies:
             error_message = f"Ready strategy not found: {self.ready_strategy}"
             raise ValueError(error_message)
