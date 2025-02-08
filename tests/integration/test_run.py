467eeeb152a8e53391266b9c034d938346f24d6c
Armes, James <james.armes@medtronic.com>
2025-02-07 22:30:47 -0700

    CI fixes

--- tests/integration/test_run.py
+++ tests/integration/test_run.py
@@ -1,10 +1,14 @@
-from pathlib import Path  # noqa: INP001
+import os  # noqa: INP001
+from pathlib import Path
 from time import sleep
+
+import pytest
 
 from process_pilot.pilot import ProcessPilot
 from process_pilot.process import ProcessManifest
 
 
+@pytest.mark.skipif("GITLAB_CI" in os.environ, reason="Skipping test that actually does process modification")
 def test_integration() -> None:
     # Load the process manifest from a JSON file
     manifest_path = Path(__file__).parent / "example.json"
